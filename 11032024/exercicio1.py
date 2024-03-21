43a9663421398f2becf85d332716e9b5c95ee497
Renan da Silva Oliveira Andrade <renan.silva3@pucpr.edu.br>
2024-03-21 11:35:58 -0300

    Arquivos da aula

---
+++ 11032024/exercicio1.py
@@ -0,0 +1,6 @@
+idade = int(input('Digite a sua idade: '))
+
+if idade >= 18:
+    print("Voce está apto a tirar carteira de habilitacao.")
+else:
+    print("Voce nao está apto a tirar carteira de habilitacao.")
---
+++ 11032024/exercicio2.py
@@ -0,0 +1,6 @@
+media = float(input('Digite a sua média: '))
+
+if media >= 7.0:
+    print('Parabéns, voce foi aprovado!')
+else:
+    print('Voce foi reprovado. :(')
